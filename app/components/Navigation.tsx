f9941723d3bd9fe8383a46b2a6d7e1d50b907a0c
Adesh Kumar <89107712+adeshkumar1@users.noreply.github.com>
2024-06-07 13:57:06 -0500

    nav bar and backgronud

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,7 +1,12 @@
 const Navigation = () => {
    return(
-      <>
-      </>
+      <div className="flex justify-between mx-16 my-4 rounded-2xl p-4 bg-gray-100">
+         <div>Adesh Kumar</div>
+         <div>Projects</div>
+         <div>Technologies</div>
+         <div>Skills</div>
+         <div>Resume</div>
+      </div>
    );
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+
+
+import Navigation from "./components/Navigation";
 
 export default function Home() {
-  return (
-    <div>
-      Test
+  return(
+    <div className="Background overflow-hidden">
+      <Navigation />
+      <div className="flex justify-center items-center mt-40">
+        <h1 className="text-2xl">Hi 👋 My Name is Adesh Kumar, I&apos;m currently studying CS @ UIUC</h1>
+      </div>
     </div>
   );
 }
